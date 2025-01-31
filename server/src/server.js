6d3b301a415369dd0646547b99d52bdd5c23755d
Hamilton <>
2025-01-31 13:59:21 +0100

    Remove server.listen and export module for serverless

--- server/src/server.js
+++ server/src/server.js
@@ -1,21 +1,19 @@
-const http = require("http");
+require('dotenv').config()
 
-require("dotenv").config();
+const app = require('./app')
+const { loadPlanetsData } = require('./models/planets.model')
+const { mongoConnect } = require('./services/mongo')
+const { loadLaunchData } = require('./models/launches.model')
 
-const app = require("./app");
-const { loadPlanetsData } = require("./models/planets.model");
-const { mongoConnect } = require("./services/mongo");
-const { loadLaunchData } = require("./models/launches.model")
-
-const PORT = process.env.PORT || 8000;
-const server = http.createServer(app);
-
-async function startServer() {
-  await mongoConnect();
-  await loadPlanetsData();
-  await loadLaunchData();
-  server.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}...`);
-  });
+async function init() {
+  await mongoConnect()
+  await loadPlanetsData()
+  await loadLaunchData()
+  console.log(`Database and data loaded`)
 }
-startServer();
+
+init().catch((err) => {
+  console.error("Error during server initialization", err);
+});
+
+module.exports = app
