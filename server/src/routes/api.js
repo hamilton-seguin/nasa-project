718d7ccf5e28c1fc550646abdc696a3a121c3ac5
Hamilton <>
2025-02-01 15:11:08 +0100

    Clean up missing const

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -3,7 +3,7 @@ const express = require("express");
 const planetsRouter = require("./planets/planets.router");
 const launchesRouter = require("./launches/launches.router");
 
-api = express.Router();
+const api = express.Router();
 
 api.use("/planets", planetsRouter);
 api.use("/launches", launchesRouter);
