0014fb506de98370057df03fa4cba704122f1d54
Hamilton <>
2025-02-01 14:48:22 +0100

    run init() only when db is loaded (delete server.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,22 @@
-const serverless = require('serverless-http');
-const app = require('../src/server');
+const serverless = require('serverless-http')
+const app = require('../src/app')
 
-module.exports = serverless(app);
+const { mongoConnect } = require('../src/services/mongo')
+const { loadPlanetsData } = require('../src/models/planets.model')
+const { loadLaunchData } = require('../src/models/launches.model')
+
+let isInitialized = false
+async function init() {
+  if (!isInitialized) {
+    await mongoConnect()
+    await loadPlanetsData()
+    await loadLaunchData()
+    console.log('Database and data loaded')
+    isInitialized = true
+  }
+}
+
+module.exports = async (req, res) => {
+  await init()
+  return serverless(app)(req, res)
+}
